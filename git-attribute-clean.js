d820207e9f61ced832684410225ef4eacf78005a
akkitheakhil <akhilpad007@gmail.com>
2021-12-24 22:23:41 +0530

    [test]

---
+++ git-attribute-clean.js
@@ -0,0 +1,18 @@
+const fsWrite = require('fs').writeFile;
+
+const environmentPaths = ['./src/environments/environment.prod.ts', './src/environments/environment.ts'];
+const envConfig = `export const environment = {
+  production: false,
+  apiBaseUrl: '',
+};
+`;
+
+environmentPaths.forEach(filePath => {
+  fsWrite(filePath, envConfig, function (err) {
+    if (err) {
+      throw console.error(err);
+    } else {
+      console.log(`Successfully created 'cleaned' environment file: ${filePath}`);
+    }
+  });
+});
